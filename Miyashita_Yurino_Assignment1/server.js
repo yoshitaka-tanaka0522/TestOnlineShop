ca58341a6ce5a8ae993ee6e2bf769320a36da4c2
yoshitaka-tanaka0522 <66899297+yoshitaka-tanaka0522@users.noreply.github.com>
2022-12-04 16:15:51 +0900

    cart function1

--- Miyashita_Yurino_Assignment1/server.js
+++ Miyashita_Yurino_Assignment1/server.js
@@ -1,62 +1,69 @@
 //created by Yurino Miyashita 
 //modified Blake Saari's server.js 
+
 // Load Express Package
-var express = require('express');
-var app = express();
+    var express = require('express');
+    var app = express();
+
 // Load Body-Parser Package
-var parser = require("body-parser");
+    var parser = require("body-parser");
+
 // Load QueryString Package
-const qs = require('querystring');
+    const qs = require('querystring');
+
 // Get Body
-app.use(parser.urlencoded({
-  extended: true
-}));
+    app.use(parser.urlencoded({
+  extended: true}));
+
 // Monitor all requests
-app.all('*', function (request, response, next) {
-  console.log(request.method + ' to ' + request.path);
-  next();
-});
+    app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+    next();
+    });
+
 // Route all other GET requests to files in public 
-app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
+
 // Load Product Data   
-var products = require(__dirname + '/products.json');
+    var products = require(__dirname + '/products.json');
+
 // Initialize Quantity
-products.forEach((prod, i) => {
-  prod.quantity_available = products[i].quantity_available
-})
+    products.forEach((prod, i) => {
+        prod.quantity_available = products[i].quantity_available
+    })
+
 //determine if there is error in quantity text box.
 //copied from invoice.html in store 1 direcotry and modified 
 function notAPosInt(arrayElement, returnErrors = false) {
   errors = []; // [] is to display below if functions,  assume no errors at first
   if (arrayElement == '') {
     arrayElement = 0;
-  }
-  //assume there was no entry on texbox
+  } 
+
+//assume there was no entry on texbox
   if (Number(arrayElement) != arrayElement) errors.push('Not a number!'); // Check if string is a number value
   if (arrayElement < 0) errors.push('Negative value!'); // Check if it is non-negative
   if (parseInt(arrayElement) != arrayElement) errors.push('Not an integer!'); // Check that it is an integer
   return (returnErrors ? errors : (errors.length == 0))
 }
+
 // Routing taking json file    
 app.get("/products.json", function (request, response, next) {
   response.type('.js');
   var products_str = `var products = ${JSON.stringify(products)};`;
   response.send(products_str);
 });
+
 // Process purchase request (validate quantities, check quantity available)
 app.post("/purchase", function (request, response, next) {
-  console.log(request.body); //getting request from invoice.html body
+  console.log(request.body);                  //getting request from invoice.html body
   var quantities = request.body['quantity']; //assigning value to quantities as quantity entred in store.html textbox
   var errors = {};
   var available_quantity = false;
   for (i in quantities) {
     console.log(quantities[i])
-    console.log(notAPosInt(quantities[i]))
     if (notAPosInt(quantities[i]) == false) {
       errors['quantity' + i] = `Please submit valid data for ${products[i].name}!` //if quantity enetred is invalid number 
-    }
-    if (!(quantities[i] === '') && Number(quantities[i]) === 0) {
-      errors['quantity' + i] = `If the ${products[i].name} quantity is 0, please enter blank!` //if the cell is filled with 0 
     }
     if (quantities[i] > 0) { //if quantity entered is greater than, meaning no errors
       available_quantity = true;
@@ -65,17 +72,18 @@ app.post("/purchase", function (request, response, next) {
       errors['quantity' + i] = `We currenly don't have ${(quantities[i])}${products[i].name}. please check our website later!`
     }
   }
+
   //taking quantity entered to display in invoice
-  let quantity_object = {
+  let quantity_object = {   
     "quantity": JSON.stringify(quantities)
-  }; //creating string by quantity_object
+  };                                                                            //creating string by quantity_object
   console.log(Object.keys(errors));
-  if (Object.keys(errors).length == 0) { //no errors, 
-    for (i in quantities) { //remove purchase quantity from inventory
+  if (Object.keys(errors).length == 0) {                                            //no errors, 
+    for (i in quantities) {                                                      //remove purchase quantity from inventory
       products[i].quantity_available -= Number(quantities[i]);
-    } //sends invoice with quantity with quary string
-    response.redirect('./invoice.html?' + qs.stringify(quantity_object)); //inserting value as quary string to invoice.html table 
-  } else { //with errors, redirect to store.html  
+    }                                                                            //sends invoice with quantity with quary string
+    response.redirect('./invoice.html?' + qs.stringify(quantity_object));       //inserting value as quary string to invoice.html table 
+  } else {                                                                      //with errors, redirect to store.html  
     let errors_obj = {
       "errors": JSON.stringify(errors)
     };
@@ -83,5 +91,6 @@ app.post("/purchase", function (request, response, next) {
     response.redirect('./store.html?' + qs.stringify(quantity_object) + '&' + qs.stringify(errors_obj)); //redirect to store.html and display errors 
   }
 });
+
 // Start server
-app.listen(5000, () => console.log(`listening on port 5000`));
+app.listen(8080, () => console.log(`listening on port 8080`));
