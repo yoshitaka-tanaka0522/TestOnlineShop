d095da1a7ef8960be8d2ba117534408aaae6a2b8
yoshitaka-tanaka0522 <66899297+yoshitaka-tanaka0522@users.noreply.github.com>
2022-12-08 15:39:13 +0900

    カート機能作成中

--- Miyashita_Yurino_Assignment3/server.js
+++ Miyashita_Yurino_Assignment3/server.js
@@ -372,20 +372,22 @@ if (Object.keys(registration_update_errors).length == 0) {
     }
 })
 
+var quantities = [];
 app.get("/add_to_cart", function (request, response) {
-  // var products_key = request.query['products_key']; // get the product key sent from the form post
-  // console.log(`products_key--${products_key}`);
-  var products_key = 'Mystery';
-  let aaa = ['1','5','2','6']
-  var quantities = aaa.map(Number);
+  var products_key = request.query['products_key']; // get the product key sent from the form post
+  console.log(`products_key--${products_key}`);
+  request.query['quantities'].map(function(quantity, i) {
+    if(quantity) {
+      quantities[i] = Number(quantity);
+    }
+  });// Get quantities from the form post and convert strings from form post to numbers
   request.session.cart[products_key] = quantities; // store the quantities array in the session cart object with the same products_key. 
-  // var quantities = request.query['quantities'].map(Number); // Get quantities from the form post and convert strings from form post to numbers
-  // console.log(`quantities--${quantities}`);
-  response.redirect('./shoppingcart.html');
+  console.log(`quantities--${quantities}`);
+  response.redirect('./store1.html');
 });
 
-app.get("/get_cart", function (request, response) {
-  response.json(request.session.cart);
+app.get("/get_cart", async function (request, response) {
+  return response.json(request.session.cart);
 });
 
 app.get("/checkout", function (request, response) {
